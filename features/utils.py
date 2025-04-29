604342bb60418ef8ba3c277fac70eb96cbff3c26
Zhiyu Wang <zcabanq@ucl.ac.uk>
2025-04-29 21:04:06 +0100

    fix sse eigen feature nan bug

--- features/utils.py
+++ features/utils.py
@@ -1,4 +1,8 @@
-def eigenval_features(evals):
+from torch import clamp
+
+
+def eigenval_features(evals, eps=1e-5):
+    evals = clamp(evals, min=eps)
     linearity = (evals[:, 0] - evals[:, 1]) / evals[:, 0]
     planarity = (evals[:, 1] - evals[:, 2]) / evals[:, 1]
     scattering = evals[:, 2] / evals[:, 0]
